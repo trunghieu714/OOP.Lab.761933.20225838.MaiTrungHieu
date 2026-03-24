18c4d74048d2b61300fe92b4272b5d560c212813
dempseyindahouse <trunghieu17112004@gmail.com>
2026-03-24 14:25:10 +0700

    Add CalculateTwoNumbers.java for bai 2.2.5

---
+++ lab1/bai2.2.5/CalculateTwoNumbers.java
@@ -0,0 +1,25 @@
+import javax.swing.JOptionPane;
+
+public class CalculateTwoNumbers {
+    public static void main(String[] args) {
+        String strNum1 = JOptionPane.showInputDialog(null, "Please input the first number: ", "Input", JOptionPane.INFORMATION_MESSAGE);
+        String strNum2 = JOptionPane.showInputDialog(null, "Please input the second number: ", "Input", JOptionPane.INFORMATION_MESSAGE);
+
+        double num1 = Double.parseDouble(strNum1);
+        double num2 = Double.parseDouble(strNum2);
+
+        String results = "First number: " + num1 + "\nSecond number: " + num2 + "\n";
+        results += "Sum: " + (num1 + num2) + "\n";
+        results += "Difference: " + (num1 - num2) + "\n";
+        results += "Product: " + (num1 * num2) + "\n";
+
+        if (num2 != 0) {
+            results += "Quotient: " + (num1 / num2);
+        } else {
+            results += "Quotient: Cannot divide by zero!";
+        }
+
+        JOptionPane.showMessageDialog(null, results, "Calculation Results", JOptionPane.INFORMATION_MESSAGE);
+        System.exit(0);
+    }
+}
