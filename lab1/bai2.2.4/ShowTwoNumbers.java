c471ff6880ca13be5ac996d381b490e3aaeae02f
dempseyindahouse <trunghieu17112004@gmail.com>
2026-03-24 14:46:01 +0700

    Update Java code

---
+++ lab1/bai2.2.4/ShowTwoNumbers.java
@@ -0,0 +1,23 @@
+import javax.swing.JOptionPane;
+public class ShowTwoNumbers {
+    public static void main(String[] args) {
+        String strNum1, strNum2;
+       String strNotification = "You've just entered: ";
+      strNum1 = JOptionPane.showInputDialog(null,
+       "Please input the first number: ", "Input",
+        JOptionPane.INFORMATION_MESSAGE);
+      strNum2 = JOptionPane.showInputDialog(null,
+       "Please input the second number: ", "Input",
+        JOptionPane.INFORMATION_MESSAGE);
+      strNotification += strNum1 + " and "; 
+      strNum2= JOptionPane.showInputDialog(null,
+       "Please input the second number: ", "Input",
+        JOptionPane.INFORMATION_MESSAGE);
+      strNotification += strNum2;
+      JOptionPane.showMessageDialog(null, strNotification,
+       "SHOW TWO NUMBERS", JOptionPane.INFORMATION_MESSAGE);
+      
+      System.exit(0);
+    }
+    
+}
